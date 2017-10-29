65cd9d680cc266649308fe1af9917774b939b029
Naomi Tague <ctague@bren.ucsb.edu>
2017-10-29 14:01:56 -0700

    added a new subroutine to compute radiation and sagehen climate data documentation

---
+++ R/sageclim.R
@@ -0,0 +1,17 @@
+#' Climate Data from Sagehen Creek
+#'
+#' Data from meterologic station
+#'
+#' @format A dataframe with 8 columns and 27274 row
+#' \itemize{
+#' \item date Month/day/year
+#' \item tmin minimum daily temperature C
+#' \item tmax maximum daily temperature C
+#' \item rain rainfall in mm
+#' \item year
+#' \item month
+#' \item day
+#' \item wy water year
+#' }
+#'
+"sageclim"
---
+++ R/sun.R
@@ -0,0 +1,15 @@
+#' Direct solar radiation at top of canopy
+#'
+#' Data from Sagehen simulation model (estimates based on latitude, day of year and solar geometry
+#' and estimates of atmospheric transmissivity)
+#'
+#'  @format A dataframe with 4 columns and 3652 row
+#' \itemize{
+#' \item Kdown_direct downwelling solar radiation (J/m2/day)
+#' \item year
+#' \item month
+#' \item day
+#' \item wy water year
+#' }
+#'
+"sun"
